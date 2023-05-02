b453f3ff429ee5ec0379c004fdc1b854713524fd
Terraform <noreply@codingones.com>
2023-05-02 22:29:02 +0200

    feat: adding terraform aws service file

--- ecr.tf
+++ ecr.tf
@@ -9,11 +9,7 @@ resource "aws_ecr_repository" "api" {
   tags = local.tags
 }
 
-data "external" "api_image" {
-  program = ["bash", "-c", "aws ecr describe-images --repository-name ${aws_ecr_repository.api.name} --query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[0]' --output text | { read image_tag; if [ -z \"$image_tag\" ]; then echo '{\"image_tag\": \"\"}'; else echo '{\"image_tag\": \"'$image_tag'\"}'; fi; }"]
-}
-
-locals {
-  api_image_tag = data.external.api_image.result["image_tag"]
-  api_image_url = length(local.api_image_tag) > 0 ? "${aws_ecr_repository.api.repository_url}:${local.api_image_tag}" : ""
+data "aws_ecr_image" "api_image" {
+  repository_name = aws_ecr_repository.api.name
+  most_recent     = true
 }
